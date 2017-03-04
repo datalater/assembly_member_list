eb16c0ae03bff36ddc10c85442af668f7447d4b5
datalater <the7mincheol@gmail.com>
2017-03-04 16:22:43 +0900

    first commit

---
+++ assembly_member_list.py
@@ -0,0 +1,27 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+import time
+
+url = "http://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do?currentPage=1&rowPerPage=1000"
+
+resp = requests.get(url)
+resp.encoding = 'utf8'
+html = resp.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+f = open(str(time.time())+".txt", 'wt', encoding='utf-8')
+f.write(html)
+
+for member_tag in soup.select('.memberna_list dl dt a'):
+    name = member_tag.text
+    link = member_tag['href']
+
+    matched = re.search(r'\d+', link)
+    if matched:
+        member_id = matched.group(0)
+    else:
+        member_id = None
+
+    print(name.encode('cp949', 'ignore').decode('cp949'), member_id) 
